c6828c0d5d4ee4f0ad071fff621b567ef3297112
Erik Ecoologic <erikecoologic@gmail.com>
2018-12-24 09:58:29 +1000

    Personal notes

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_12_22_034637) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.integer "role", limit: 2, default: 0, null: false
+    t.text "private_notes", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
