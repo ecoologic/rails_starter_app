d66b8fd900b20ee1241c5107659b56f1cd8b3f3f
Erik Ecoologic <erikecoologic@gmail.com>
2019-01-03 22:49:04 +1000

    About and T&C

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,6 +2,7 @@ class HomePagesController < ApplicationController
   skip_authorization_check
 
   def index
+    @title = SETTINGS[:name]
     flash.now[:notice] = "Welcome! Log in to do something (not really)" unless user_signed_in?
   end
 end
---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,10 @@
+# Static pages deserve helpers too, it will make change easier
+module StaticPagesHelper
+  def about_page_path
+    '/about'
+  end
+
+  def terms_and_conditions_path
+    '/terms_and_conditions'
+  end
+end
