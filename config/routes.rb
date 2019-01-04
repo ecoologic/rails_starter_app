d3a2bd62e4efa88b34ac0eebdd29f67fef49f755
Erik Ecoologic <erikecoologic@gmail.com>
2019-01-04 23:28:05 +1000

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :pages
   root 'home_pages#index'
 
   devise_for :users
 
   resources :users, only: %i[index show edit update] # Rest done by Devise
+  resources :pages
 end
--- db/migrate/20181225073702_create_pages.rb
+++ db/migrate/20181225073702_create_pages.rb
@@ -1,7 +1,9 @@
 class CreatePages < ActiveRecord::Migration[5.2]
   def change
     create_table :pages do |t|
-      t.string :name, null: false, index: true, unique true
+      t.references :user, null: false, index: true
+
+      t.string :name, null: false, index: true, unique: true
       t.string :url, null: false, index: true, unique: true
       t.text :html, null: false, default: ''
 
