0e835d3107c70311f10f8a91765acb6cbe3421b2
Erik Ecoologic <erikecoologic@gmail.com>
2019-01-12 00:29:16 +1000

    Listable: manageable lists in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # Extend all your Controllers from here
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  # Prepare for listing on a page (eg: table/ul/li)
+  # NOTE: Remember to add an index to updated_at in every model
+  #       where you intend to leave this default
+  def listable(scope)
+    scope.ordered.limit(10)
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  scope :ordered, -> { order(updated_at: :desc) }
+
   def to_s
     "#{self.class}(#{id || '#' + object_id})"
   end
