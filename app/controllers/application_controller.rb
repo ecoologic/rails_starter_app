29a0cb02b85dc07c2733ee4dd90e05729f814c25
Erik Ecoologic <erikecoologic@gmail.com>
2019-01-12 00:29:16 +1000

    Listable: manageable lists in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # Extend all your Controllers from here
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  # Prepare for listing on a page (eg: table/ul/li)
+  def listable(scope)
+    scope.ordered.limit(10)
+  end
 end
